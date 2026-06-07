4a7eb0f6f642737b696762c4f527754b6f92cce6
karthick-kk <kkzone@gmail.com>
2026-06-07 11:31:24 +0530

    sm8750-common: Remove unused vibratorext_shim
    The shim approach was not needed - stock Xiaomi vibratorfeature HAL
    works correctly with proper props and firmware files.

--- common.mk
+++ common.mk
@@ -295,10 +295,8 @@ PRODUCT_SOONG_NAMESPACES += \
 # Binary patched to register as IVibrator/default instead of vibratorfeature
 # LD_PRELOAD shim intercepts AIBinder_setExtension and registers IVibratorExt as named service
 PRODUCT_PACKAGES += \
-    libvibratorext_shim
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/vibratorext_shim/vendor.xiaomi.hardware.vibratorfeature.service.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/vendor.xiaomi.hardware.vibratorfeature.service.rc
 # Dolby
 PRODUCT_SOONG_NAMESPACES += hardware/dolby
 PRODUCT_PACKAGES += LunarisDolby
